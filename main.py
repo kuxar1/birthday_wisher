6243afaee271086a6c9d86b55247c3c1d780804a
oleksandr.kuk <oleksandr.kuk@partsid.com>
2022-01-14 20:12:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import datetime as dt
+import smtplib
+import pandas
+import random
+
+now = dt.datetime.now()
+day = now.day
+month = now.month
+
+MY_EMAIL = 'dengerskif3@gmail.com'
+PASSWORD = 'kuxar9152'
+
+data = pandas.read_csv("birthdays.csv")
+data_dict = data.to_dict('records')
+
+letter_list = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+random_letter = random.choice(letter_list)
+
+for i in range(len(data_dict)):
+    if data_dict[i]["month"] == month and data_dict[i]["day"]:
+        name = data_dict[i]["name"]
+        with open(f"letter_templates/{random_letter}") as file:
+            letter = file.read()
+            letter = letter.replace("[NAME]", name)
+
+with smtplib.SMTP('smtp.gmail.com') as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs='dengerskif2@gmail.com',
+        msg=f'Subject:Happy Birthday \n\n {letter}'
+    )
